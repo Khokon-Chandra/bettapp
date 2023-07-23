57dc1a81648f42ee506bbc517679ee2b6284180d
unknown <khokonchandra4@gmail.com>
2023-07-24 02:21:36 +0600

    Illuminate database added

---
+++ src/DB.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Riazul\Riaz;
+
+use Illuminate\Database\Capsule\Manager;
+use Illuminate\Support\Traits\CapsuleManagerTrait;
+
+class DB extends Manager
+
+{
+    // use CapsuleManagerTrait;
+}
